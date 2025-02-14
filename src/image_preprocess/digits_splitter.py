5f52645e888cfb85faf261197daa4523dfeda568
troegubov.aiu <troegubov.aiu@inviewlab.com>
2025-02-14 17:12:00 +0300

    refactor(digits_splitter.py): refactoring code

--- src/image_preprocess/digits_splitter.py
+++ src/image_preprocess/digits_splitter.py
@@ -21,7 +21,13 @@ class DigitsSplitter(ABC):
 
 class SimpleSplitter(DigitsSplitter):
 
+    # relative to cell width
     MARGIN = 0.05
+    # relative to cell area
+    MIN_AREA_COEFF = 1 / 36
+    # relative to cell height
+    MIN_ARC_COEFF = 1.0
+    WHITE = 255
 
     def __init__(self):
         super().__init__()
@@ -33,52 +39,15 @@ class SimpleSplitter(DigitsSplitter):
         x_step = width / 9
         y_step = height / 9
 
-        # some margins to remove rest of grid
-        m_y = self.MARGIN * y_step
-        m_x = self.MARGIN * x_step
-
-        lst_digits: list[np.ndarray | None] = []
+        lst_digits = []
 
         for i_y, y in enumerate(np.arange(0, height, y_step)):
             for i_x, x in enumerate(np.arange(0, width, x_step)):
 
-                y1 = int(y + m_y)
-                y2 = int(y + y_step - m_y)
-
-                x1 = int(x + m_x)
-                x2 = int(x + x_step - m_x)
-
-                # binary image here after extractor
-                roi = image[y1:y2, x1:x2].copy()
-                min_area = 1 / 36 * np.prod(roi.shape)
-                min_arc = roi.shape[0]
-
-                # take external contours, find perimetr and area
-                cntrs, _ = cv.findContours(roi, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-                cntrs_area = np.array([cv.contourArea(cntr) for cntr in cntrs])
-                cntrs_arcs = np.array([cv.arcLength(cntr, closed=True) for cntr in cntrs])
-
-                # mask to remove too small contours as noise:
-                cntrs_mask = np.zeros_like(roi)
-                noise_contours = [
-                    cntr
-                    for cntr, arc, area in zip(cntrs, cntrs_area, cntrs_arcs)
-                    if area < min_area and arc < min_arc
-                ]
-                cv.drawContours(cntrs_mask, noise_contours, -1, color=255, thickness=cv.FILLED)
-
-                roi = cv.bitwise_and(roi, cv.bitwise_not(cntrs_mask))
-
-                if logger.isEnabledFor(logging.DEBUG):
-                    image[y1:y2, x1:x2] = roi
-
-                cntrs, _ = cv.findContours(roi, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-                cntrs_area = np.array([cv.contourArea(cntr) for cntr in cntrs])
-
-                digit = None if len(cntrs) == 0 or len(cntrs) > 4 else roi
+                proc_roi, digit = self._extract_digit(image, x, y)
 
                 save_debug_image(
-                    roi,
+                    proc_roi,
                     Path(self.filestem + f"_{i_y}_{i_x}_{digit is not None}").with_suffix(".jpg"),
                     logging.DEBUG,
                 )
@@ -89,6 +58,51 @@ class SimpleSplitter(DigitsSplitter):
         )
 
         return lst_digits
+
+    def _extract_digit(
+        self, image: np.ndarray, x: float, y: float
+    ) -> tuple[np.ndarray, np.ndarray | None]:
+        """Extracts a single digit from a given cell region."""
+        # cell size
+        y_step, x_step = image.shape[0] / 9, image.shape[1] / 9
+        # margins
+        m_y, m_x = self.MARGIN * y_step, self.MARGIN * x_step
+        # roi rectangle
+        x1, y1 = int(x + m_x), int(y + m_y)
+        x2, y2 = int(x + x_step - m_x), int(y + y_step - m_y)
+        # binary image here after extractor
+        roi = image[y1:y2, x1:x2].copy()
+        roi = self._remove_noise(roi)
+
+        if logger.isEnabledFor(logging.DEBUG):
+            image[y1:y2, x1:x2] = roi
+
+        cntrs, _ = cv.findContours(roi, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        digit = None if len(cntrs) == 0 or len(cntrs) > 4 else roi
+
+        return roi, digit
+
+    def _remove_noise(self, roi: np.ndarray) -> np.ndarray:
+        """Removes small noise elements from the image using contours filtering."""
+        min_area = self.MIN_AREA_COEFF * np.prod(roi.shape)
+        min_arc = self.MIN_ARC_COEFF * roi.shape[0]
+
+        # take external contours, find perimetr and area
+        cntrs, _ = cv.findContours(roi, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        # mask to remove too small contours as noise:
+        noise_mask = np.zeros_like(roi)
+
+        noise_cntrs = [
+            cntr
+            for cntr in cntrs
+            if cv.contourArea(cntr) < min_area and cv.arcLength(cntr, closed=True) < min_arc
+        ]
+        cv.drawContours(noise_mask, noise_cntrs, -1, color=self.WHITE, thickness=cv.FILLED)
+
+        roi = cv.bitwise_and(roi, cv.bitwise_not(noise_mask))
+        roi = cv.morphologyEx(roi, cv.MORPH_CLOSE, kernel=np.ones((3, 3)))
+
+        return roi
 
     def __call__(self, image: np.ndarray, with_not_nones: bool = False):
 
